156380a603cf9d6cfae4a6311bc90e845ae1c763
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-01-25 18:25:55 +0100

    fetch friends list on connect

--- src/scripts/index.min.js
+++ src/scripts/index.min.js
@@ -5,7 +5,24 @@ $(document).ready($ => {
     socket.emit("auth", { userKey: "bruh" });
 
     socket.on("response", res => {
+
         console.log(res);
+
+        if (res.status) {
+            socket.emit("data", "user:fetchFriendsList");
+        }
+    });
+
+    socket.on("data", (type, data) => {
+        switch (type) {
+            case "friendsList":
+
+                console.log(data);
+
+                break;
+            default:
+                break;
+        }
     });
 
 });
