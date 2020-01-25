d3ae55737859cd8cd147ae8ce72dcf4fe3aab7cc
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-01-25 17:28:41 +0100

    client connect to websocket

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-const socket = require("socket.io-client").connect("http://localhost:3000", { transports: ["websocket"] });
+const socket = io.connect("http://localhost:3000", { transports: ["websocket"] });
 
 $(document).ready($ => {
 
