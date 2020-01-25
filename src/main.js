dd6f59c1745a5bd89c08ebd7a4fcdb84c479c04c
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-01-25 17:01:42 +0100

    added main process script

--- src/main.js
+++ src/main.js
@@ -2,3 +2,38 @@
 
 const path = require("path");
 const electron = require("electron");
+const { app, remote, BrowserWindow, Menu, ipcMain, globalShortcut } = electron;
+
+const singleInstanceLock = app.requestSingleInstanceLock();
+app.setAppUserModelId("com.gizmo.chat");
+
+var mainWindow;
+
+if (singleInstanceLock) {
+
+    app.on("second-instance", () => {
+        if (mainWindow) {
+            mainWindow.restore();
+            mainWindow.focus();
+        }
+    });
+
+    app.on("ready", () => {
+
+        mainWindow = new BrowserWindow({
+            minWidth: 1200,
+            minHeight: 700,
+            frame: false,
+            backgroundColor: "#222222",
+            webPreferences: {
+                nodeIntegration = true
+            }
+        });
+
+        mainWindow.loadFile("src/interface/index.html");
+
+    });
+
+} else {
+    app.quit();
+}
