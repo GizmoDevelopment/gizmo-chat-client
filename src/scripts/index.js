d3abb3c79a34dca086481bbe410305bcbab970a5
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-02-01 17:37:56 +0100

    Display friends in sidebar + handle click on em

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,11 @@
+// Modules //
 const socket = io.connect("http://localhost:3000", { transports: ["websocket"] });
+
+// Storage //
+var friendsList = {};
+
+// Currents //
+var currentConversation;
 
 $(document).ready($ => {
 
@@ -18,11 +25,27 @@ $(document).ready($ => {
             case "friendsList":
 
                 console.log(data);
+                friendsList = data;
+
+                for (let key in data) {
+                    if (typeof data[key] == "object") {
+                        $("#sideBar").append(`
+                            <div id = "sideBarUser-${data[key].user_id}" class = "sideBarUser">
+                                <img src = "https://www.gizmo.moe/uploads/avatars/${data[key].user_avatar}" draggable = false>
+                            </div>
+                        `);
+                    }
+                }
 
                 break;
             default:
                 break;
         }
+    });
+
+    $("#sideBar").on("click", ".sideBarUser", e => {
+        closeConversation();
+        openConversation(e.target.id);
     });
 
 });
