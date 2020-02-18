151d8dbdc89d8366c5f3eabeb459a32beb5ac413
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-02-18 21:50:39 +0100

    create tooltip

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -4,3 +4,21 @@ const path = require("path"),
     $ = require("../modules/jquery.min.js"),
     electron = require("electron"),
     { remote, ipcRenderer } = electron;
+
+function createTooltip (parent, pos, data) {
+
+    console.log(parent);
+    console.log(data);
+
+    parent.append(`
+        <div class = "tooltip">
+            ${data}
+        </div>
+    `);
+
+}
+
+function removeTooltip (parent) {
+    console.log(parent);
+    parent.children("#tooltip").remove();
+}
