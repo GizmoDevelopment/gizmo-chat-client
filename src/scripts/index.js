711d1ec6d9c5abfe4ab0af7b3face249052f587f
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-04-07 22:51:53 +0200

    friends list load function + read userKey from env

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,71 +1,72 @@
 "use strict";
+require("dotenv").config({ path: path.join(__core, ".env") }); // Throw out before release
 
 // Modules //
 const socket = io.connect("http://localhost:3000", { transports: ["websocket"] });
 
 // Storage //
-var friendsList = {};
+var userData = {};
+var friendsList = new Map();
 
-// Currents //
-var currentConversation;
+function resolveId (id) {
+    return id.match(/friendsListUser-(.*)/)[1];
+}
+
+function loadFriendsList () {
+    socket.emit("data", "user:fetchFriendsList", data => {
+        if (typeof data === "object") {
+
+            $("#friendsList").empty();
+
+            for (var key in data) {
+                if (typeof data[key] === "object") {
+
+                    friendsList.set(key, data[key]);
+
+                    $("#friendsList").append(`
+                        <div id = "friendsListUser-${data[key].user_id}" class = "friendsListUser">
+                            <img src = "https://www.gizmo.moe/uploads/avatars/${data[key].user_avatar}" draggable = false>
+                            <span class = "friendUsername">${data[key].user_uid}</span>
+                            <!--<div class = "friendControlButtons">
+                                <div class = "button">X</div>
+                            </div>-->
+                        </div>
+                    `);
+                }
+            }
+        }
+    });
+}
 
 $(document).ready($ => {
 
-    socket.emit("auth", { userKey: "a" });
+    socket.emit("auth", { userKey: process.env.USER_KEY }); // Throw out before release
 
     socket.on("response", res => {
 
         console.log(res);
 
-        if (res.status) {
-            socket.emit("data", "user:fetchFriendsList");
+        if (res.status && res.data) {
+            userData = res.data;
+            loadFriendsList();
+        } else {
+            console.error("Didn't receive a proper authentication response");
         }
     });
 
     socket.on("data", (type, data) => {
         switch (type) {
-            case "friendsList":
-
-                console.log(data);
-                friendsList = data;
-
-                for (let key in data) {
-                    if (typeof data[key] == "object") {
-                        $("#friendsList").append(`
-                            <div id = "friendsListUser-${data[key].user_id}" class = "friendsListUser">
-                                <img src = "https://www.gizmo.moe/uploads/avatars/${data[key].user_avatar}" draggable = false>
-                                <span class = "friendUsername">${data[key].user_uid}</span>
-                                <!--<div class = "friendControlButtons">
-                                    <div class = "button">X</div>
-                                </div>-->
-                            </div>
-                        `);
-                    }
-                }
-
-                break;
             default:
                 break;
         }
     });
 
-    /*
-        $("#sideBar").on("click", ".sideBarUser", e => {
-            closeConversation();
-            openConversation(e.target.id);
-        });
-
-        $("#sideBar").on("mouseenter", ".sideBarUser", e => {
-            createTooltip(e.target, "right", e.target.id);
-        });
-
-        $("#sideBar").on("mouseleave", ".sideBarUser", e => {
-            removeTooltip(e.target);
-        });
-    */
-
     $("#sideBar").on("click", "#friendsButton", e => {
         $("#friendsList").css("visible") ? $("#friendsList").hide() : $("#friendsList").show();
+    });
+
+    $("#friendsList").on("click", ".friendsListUser", (e) => {
+        launchConversation(resolveId(e.target.id));
     });
 
 });
